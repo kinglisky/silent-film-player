0afd654d24de8d2113799febe691d56dd853d314
芊野 <free.nan.sky@gmail.com>
2020-08-25 17:20:05 +0800

    chore: update ts url

--- video-data.js
+++ video-data.js
@@ -1,5 +1,5 @@
 export default {
     width: 1920,
     height: 1080,
-    url: `https://kinglisky.oss-cn-hangzhou.aliyuncs.com/videos/hanabi.ts`,
+    url: 'https://kinglisky.oss-cn-hangzhou.aliyuncs.com/videos/fireworks.ts',
 };
